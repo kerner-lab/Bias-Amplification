f7404c6a98a6934c76e4858dc31d154a93a26678
Ushnesha Daripa <udaripa@asu.edu>
2025-11-05 13:21:55 -0700

    added test functions for D_bias, M_unbias, M2

--- tests/test_datacreator.py
+++ tests/test_datacreator.py
@@ -1,6 +1,6 @@
-from utils.datacreator import dataCreator
+from utils.datacreator import dataCreator, validate_error_percent
 import numpy as np
-
+import pytest
 class TestingBasicFunctionality:
 
     def test_datacreator_returns_correct_outputs(self):
@@ -16,6 +16,23 @@ class TestingBasicFunctionality:
         assert len(M_unbias) == N, f"M_unbias should have length {N}"
         assert len(M2) == N, f"M2 should have length {N}"
 
+    def test_validate_error_percent_on_correct_values(self):
+        try:
+            validate_error_percent(0, "error")
+            validate_error_percent(0.15, "error")
+            validate_error_percent(0.25, "error")
+            assert True, "validate_error_percent should not raise an error"
+        except ValueError:
+            pytest.fail("validate_error_percent() raised Valueerror unexpectedly")
+
+    def test_validate_error_percent_on_negative_values(self):
+        with pytest.raises(ValueError):
+            validate_error_percent(-0.1, "error")
+
+    def test_validate_error_percent_on_high_values(self):
+        with pytest.raises(ValueError):
+            validate_error_percent(0.8, "error")
+
 class TestingBiasValidation:
 
     def test_datacreator_returns_correct_P(self):
@@ -28,12 +45,75 @@ class TestingBiasValidation:
     def test_datacreator_returns_correct_D(self):
         N = 128
         q1 = N//4
-        q2 = N//2
+        mid = N//2
         q3 = 3*N//4
         _, D, _, _, _ = dataCreator(N=N, error_percent=0.1)
         assert np.sum(D[:q1]==0) == q1, f"D should have {q1} 0s in the first quarter"
-        assert np.sum(D[q1:q2]==1) == q1, f"D should have {q1} 1s in the second quarter"
-        assert np.sum(D[q2:q3]==0) == q1, f"D should have {q1} 0s in the third quarter"
+        assert np.sum(D[q1:mid]==1) == q1, f"D should have {q1} 1s in the second quarter"
+        assert np.sum(D[mid:q3]==0) == q1, f"D should have {q1} 0s in the third quarter"
         assert np.sum(D[q3:]==1) == q1, f"D should have {q1} 1s in the fourth quarter"
+
+    def test_datacreator_returns_correct_D_bias(self):
+        N = 16384
+        data_err_pct = 0.05
+        data_err_cnt = int(N * data_err_pct)
+        _, D, D_bias, _, _ = dataCreator(N=N, data_error_percent=data_err_pct)
+        q1 = N//4
+        mid = N//2
+        q3 = 3*N//4
+        #check the count of errors
+        tot_errors_in_D_bias = np.sum(D != D_bias)
+        errors_in_quarter_1 = np.sum(D[0:q1] != D_bias[0:q1])
+        errors_in_quarter_2 = np.sum(D[q1:mid] != D_bias[q1:mid])
+        errors_in_quarter_3 = np.sum(D[mid:q3] != D_bias[mid:q3])
+        errors_in_quarter_4 = np.sum(D[q3:] != D_bias[q3:])
+        #asserting tests
+        assert tot_errors_in_D_bias == 2*data_err_cnt, f"D_bias should have a total of {2*data_err_cnt} errors"
+        assert errors_in_quarter_1 == errors_in_quarter_4 == data_err_cnt, f"D_bias should have {data_err_cnt} errors in quarter 1 and 4"
+        assert errors_in_quarter_2 == errors_in_quarter_3 == 0, f"D_bias should have zero errors in quarter 2 and 3"
+
+    def test_datacreator_returns_correct_M_unbias(self):
+        N = 16384
+        err_pct = 0.2
+        err_cnt_half = int(N * (err_pct/2))
+        tot_err_cnt = err_cnt_half * 2
+        _, D, _, M_unbias, _ = dataCreator(N=N, error_percent=err_pct)
+        q1 = N//4
+        mid = N//2
+        q3 = 3*N//4
+        #check the count of errors
+        tot_errors_in_M_unbias = np.sum(D != M_unbias)
+        errors_in_quarter_1 = np.sum(D[0:q1] != M_unbias[0:q1])
+        errors_in_quarter_2 = np.sum(D[q1:mid] != M_unbias[q1:mid])
+        errors_in_quarter_3 = np.sum(D[mid:q3] != M_unbias[mid:q3])
+        errors_in_quarter_4 = np.sum(D[q3:] != M_unbias[q3:])
+        #asserting tests
+        assert tot_errors_in_M_unbias == tot_err_cnt, f"M_unbias should have a total of {tot_err_cnt} errors"
+        assert errors_in_quarter_1 == errors_in_quarter_2 == err_cnt_half//2, f"M_unbias should have {err_cnt_half//2} errors in quarter 1 and 2"
+        assert errors_in_quarter_3 == errors_in_quarter_4 == err_cnt_half - err_cnt_half//2, f"M_unbias should have {err_cnt_half - err_cnt_half//2} errors in quarter 3 and 4"
+
+
+    def test_datacreator_returns_correct_M2(self):
+        N = 16384
+        err_pct = 0.2
+        err_cnt_half = int(N * (err_pct/2))
+        tot_err_cnt = err_cnt_half * 2
+        _, D, _, _, M2 = dataCreator(N=N, error_percent=err_pct)
+        q1 = N//4
+        mid = N//2
+        q3 = 3*N//4
+        #check the count of errors
+        tot_errors_in_M2 = np.sum(D != M2)
+        errors_in_half_1 = np.sum(D[0:mid] != M2[0:mid])
+        errors_in_half_2 = np.sum(D[mid:] != M2[mid:])
+        errors_in_quarter_1 = np.sum(D[0:q1] != M2[0:q1])
+        errors_in_quarter_2 = np.sum(D[q1:mid] != M2[q1:mid])
+        errors_in_quarter_3 = np.sum(D[mid:q3] != M2[mid:q3])
+        errors_in_quarter_4 = np.sum(D[q3:] != M2[q3:])
+        #asserting tests
+        assert tot_errors_in_M2 == tot_err_cnt, f"M2 should have a total of {tot_err_cnt} errors"
+        assert errors_in_half_1 == errors_in_half_2 == err_cnt_half, f"M2 should have {err_cnt_half} errors in each half"
+        assert errors_in_quarter_1 == errors_in_quarter_4 == err_cnt_half, f"M2 should have {err_cnt_half} errors in quarter 1 and 4"
+        assert errors_in_quarter_2 == errors_in_quarter_3 == 0, f"M2 should have zero errors in quarter 2 and 3"
 
     
--- utils/datacreator.py
+++ utils/datacreator.py
@@ -16,49 +16,6 @@ def validate_error_percent(error_percent, name):
         raise ValueError(f"{name}_percent needs to be a float value in [0,0.25].")
 
 
-# # dataCreator function
-# def dataCreator(N=512, error_percent=0.1, shuffle=False, data_error_percent=None):
-#     error_percent = error_percent / 2
-#     if N < 3 or type(N) != int:
-#         raise ValueError("Expected Integer Value about 2 for N.")
-#     validate_error_percent(error_percent, "error")
-#     if data_error_percent == None:
-#         data_error_percent = error_percent
-#     else:
-#         validate_error_percent(data_error_percent, "data_error")
-#     P = np.zeros(N)
-#     P[N // 2 :] = 1
-#     D = np.zeros(N)
-#     D[N // 4 : N // 2] = 1
-#     D[3 * N // 4 :] = 1
-#     M_unbias = D.copy()
-#     M2 = D.copy()
-#     D_bias = D.copy()
-#     num_errors = int(N * error_percent)
-#     num_data_errors = int(N * data_error_percent)
-#     A_pos = np.array([i for i in range(0, N // 4)])
-#     C_pos = np.array([i for i in range(N // 2, 3 * N // 4)])
-#     A_swaps_1 = np.random.choice(A_pos, num_errors // 2, replace=False)
-#     A_swaps_2 = np.random.choice(A_pos, num_errors, replace=False)
-#     C_swaps_1 = np.random.choice(C_pos, num_errors - num_errors // 2, replace=False)
-#     A_swaps_bias = np.random.choice(A_pos, num_data_errors, replace=False)
-#     M_unbias[A_swaps_1] = 1
-#     M_unbias[A_swaps_1 + (N // 4)] = 0
-#     M_unbias[C_swaps_1] = 1
-#     M_unbias[C_swaps_1 + (N // 4)] = 0
-#     M2[A_swaps_2] = 1
-#     M2[A_swaps_2 + (3 * N // 4)] = 0
-#     D_bias[A_swaps_bias] = 1
-#     D_bias[A_swaps_bias + (3 * N // 4)] = 0
-#     if shuffle:
-#         permut = np.random.permutation(N)
-#         P = P[permut]
-#         D = D[permut]
-#         M_unbias = M_unbias[permut]
-#         M2 = M2[permut]
-#     return P, D, D_bias, M_unbias, M2
-    
-
 # dataCreator function
 def dataCreator(N=512, error_percent=0.1, shuffle=False, data_error_percent=None):
     """
